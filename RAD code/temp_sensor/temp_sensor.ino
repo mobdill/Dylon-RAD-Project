5c97cf156261f1120bf3fe596c3bd722bc4ecac1
Dylon Hill 18 <DylonHill18@newmanschool.org>
2017-08-30 14:11:35 -0500

    Temp sensor

--- RAD code/temp_sensor/temp_sensor.ino
+++ RAD code/temp_sensor/temp_sensor.ino
@@ -1,6 +1,6 @@
-int tempPin = 1;        //the analog pin the TMP36's Vout (sense) pin is connected to
+int tempPin = 1;       
                        
-int tempReading;        // the analog reading from the sensor
+int tempReading;        
  
 void setup(void) {
   
@@ -18,20 +18,16 @@ void loop(void) {
   Serial.print("Temp reading = ");
   Serial.print(tempReading);     
  
-  // converting that reading to voltage, which is based off the reference voltage
-  float voltage = tempReading * analogReference; //analogReference_voltage
+  float voltage = tempReading * analogReference; 
   voltage /= 1024.0; 
  
-  //print out the voltage
   Serial.print(" - ");
   Serial.print(voltage); Serial.println(" volts");
  
-  //print out the temperature
   float temperatureC = (voltage - 0.5) * 100 ;  
                                               
   Serial.print(temperatureC); Serial.println(" degrees C");
  
-  //conversion to Fahrenheight
   float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
   Serial.print(temperatureF); Serial.println(" degrees F");
  
