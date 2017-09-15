267db1b595f1ef505c9b953097632c9e8b72a617
Dylon Hill 18 <DylonHill18@newmanschool.org>
2017-09-14 21:13:40 -0500

    update

--- RAD code/light.py
+++ RAD code/light.py
@@ -1,4 +1,7 @@
-import RPI.GPIO as Prog1
-Prog1.setmode (Prog1.BOARD)
-Prog1.setup (7, Prog1.OUT)
-Prog1.output (7, True)
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
+
+for i in range(0,5):
+    print GPIO.input(4)
