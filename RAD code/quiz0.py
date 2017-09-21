fd1d369afa53618b42e292136b6ec69884604543
Dylon Hill 18 <DylonHill18@newmanschool.org>
2017-09-21 15:56:55 -0500

    update

---
+++ RAD code/quiz0.py
@@ -0,0 +1,18 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode (GPIO.BCM)
+GPIO.setup (4, GPIO.OUT)
+GPIO.setup (1, GPIO.IN pull_up_down=GPIO.PUD_UP)
+
+
+ while True:
+    ledblink (2)
+    ledblink (3)
+    input_state = GPIO.input (1)
+
+def ledblink(t):
+     GPIO.OUTPUT (4, GPIO.HIGH)
+     time.sleep (t)
+     GPIO.OUTPUT (4, GPIO.LOW)
+     time.sleep (t)
