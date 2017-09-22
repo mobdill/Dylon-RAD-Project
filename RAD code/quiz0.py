bbe90c8ba5806afba420919658b1fb62edd09138
Dylon Hill 18 <DylonHill18@newmanschool.org>
2017-09-22 11:43:38 -0500

    update

--- RAD code/quiz0.py
+++ RAD code/quiz0.py
@@ -10,6 +10,10 @@ GPIO.setup (1, GPIO.IN pull_up_down=GPIO.PUD_UP)
     ledblink (2)
     ledblink (3)
     input_state = GPIO.input (1)
+    if input_state == True:
+        ledblink(3);
+    else:
+        GPIO.OUTPUT (4, GPIO.LOW)
 
 def ledblink(t):
      GPIO.OUTPUT (4, GPIO.HIGH)
--- RAD code/temp.py
+++ RAD code/temp.py
@@ -1,27 +1,21 @@
-import os
-import glob
+import RPi.GPIO as GPIO
+import dht11
 import time
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
-def read_temp_raw():
-f = open(device_file, 'r')
-lines = f.readlines()
-f.close()
-return lines
-def read_temp():
-lines = read_temp_raw()
-while lines[0].strip()[-3:] != 'YES':
-time.sleep(0.2)
-lines = read_temp_raw()
-equals_pos = lines[1].find('t=')
-if equals_pos != -1:
-temp_string = lines[1][equals_pos+2:]
-temp_c = float(temp_string) / 1000.0
-temp_f = temp_c * 9.0 / 5.0 + 32.0
-return temp_c, temp_f
+import datetime
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 17
+instance = dht11.DHT11(pin=17)
+
 while True:
-print(read_temp())
-time.sleep(1)
+    result = instance.read()
+    if result.is_valid():
+        print("Last valid input: " + str(datetime.datetime.now()))
+        print("Temperature: %d C" % result.temperature)
+        print("Temperature: %d F" % result.temperature((9/5)*32))
+        print("Humidity: %d %%" % result.humidity)
+
+    time.sleep(1)
