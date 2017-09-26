0bb69a38b9d0594e89064612a7450c8788819449
Dylon Hill 18 <DylonHill18@newmanschool.org>
2017-09-25 20:53:49 -0500

    update

---
+++ RAD code/tutorial.py
@@ -0,0 +1,207 @@
+Python 3.5.4 (v3.5.4:3f56838, Aug  8 2017, 02:07:06) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> for i in range (0,5):
+	print (i)
+
+	
+0
+1
+2
+3
+4
+>>> shoppingList = ["milk", "eggs
+		
+SyntaxError: EOL while scanning string literal
+>>> shoppingList = ["milk", "eggs", "orange"]
+>>> for i in shoppingList:
+	print(i)
+
+	
+milk
+eggs
+orange
+>>> for i in tup:
+	print(i)
+
+	
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    for i in tup:
+NameError: name 'tup' is not defined
+>>> tup = (2,43,6)
+>>> for i in tup:
+	print (i)
+
+	
+2
+43
+6
+>>> for i in range (0, 101,2):
+	print (i)
+
+	
+0
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+38
+40
+42
+44
+46
+48
+50
+52
+54
+56
+58
+60
+62
+64
+66
+68
+70
+72
+74
+76
+78
+80
+82
+84
+86
+88
+90
+92
+94
+96
+98
+100
+>>> for i in range (100, 0, -1):
+	print (i)
+
+	
+100
+99
+98
+97
+96
+95
+94
+93
+92
+91
+90
+89
+88
+87
+86
+85
+84
+83
+82
+81
+80
+79
+78
+77
+76
+75
+74
+73
+72
+71
+70
+69
+68
+67
+66
+65
+64
+63
+62
+61
+60
+59
+58
+57
+56
+55
+54
+53
+52
+51
+50
+49
+48
+47
+46
+45
+44
+43
+42
+41
+40
+39
+38
+37
+36
+35
+34
+33
+32
+31
+30
+29
+28
+27
+26
+25
+24
+23
+22
+21
+20
+19
+18
+17
+16
+15
+14
+13
+12
+11
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+>>> for i in range (1,6):
+	print (i)
+
+	
+1
+2
+3
+4
+5
+>>> 
