ad56b2708b51120783c2688aa79237adea1c4f72
Dylon Hill 18 <DylonHill18@newmanschool.org>
2017-09-26 13:33:44 -0500

    update

---
+++ RAD code/tutorial(while).py
@@ -0,0 +1,16 @@
+Python 3.5.4 (v3.5.4:3f56838, Aug  8 2017, 02:07:06) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> counter = 5
+>>> print (counter)
+5
+>>> while counter < 10:
+	print (counter)
+	counter = counter + 1
+
+	
+5
+6
+7
+8
+9
+>>> 
