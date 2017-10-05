338af9bfa395b610d207e6f41296539c59cbb66b
Dylon Hill 18 <DylonHill18@newmanschool.org>
2017-10-04 21:52:01 -0500

    update

---
+++ RAD code/tutorial(stuff).py
@@ -0,0 +1,65 @@
+Python 3.5.4 (v3.5.4:3f56838, Aug  8 2017, 02:07:06) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> try:
+	if name>3:
+		print("Hi")
+
+except:
+	print("Python ran into an error. Please take a look at your code again.")
+
+	
+Python ran into an error. Please take a look at your code again.
+>>> 
+== RESTART: C:/Users/dylonhill18/Documents/Code/RAD code/tutorial(stuff).py ==
+Python ran into an error. Please take a look at your code again.
+>>> string = "lakjdlkf".
+>>>string
+SyntaxError: invalid syntax
+>>> string = "lfkjalk"
+>>> string
+'lfkjalk'
+>>> blueorangeswithcheese = "a"
+>>> cheesefrieddumplingswithcherrysauce = "b"
+>>> if blueorangeswithcheese ==/ cheesefrieddumplingswithcherrysauce:
+	
+SyntaxError: invalid syntax
+>>> if blueorangeswithcheese\==\ cheesefrieddumplingswithcherrysauce:
+	
+SyntaxError: unexpected character after line continuation character
+>>> if blueorangeswithcheese\
+   ==\
+   cheesefrieddumplingswithcherrysauce:
+	print("hello")
+else:
+	print("hiya")
+
+	
+hiya
+>>> for i in range (0,5):
+	print("hi")
+
+	
+hi
+hi
+hi
+hi
+hi
+>>> def functionName
+SyntaxError: invalid syntax
+>>> def functionName():
+	for i in range (0,5):
+		print("hi")
+
+		
+>>> functionName()
+hi
+hi
+hi
+hi
+hi
+>>> def addNum(firstNum, secondNum):
+	return (firstNum + secondNum)
+
+>>> addNum(67,2)
+69
+>>> 
