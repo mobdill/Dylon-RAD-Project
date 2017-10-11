c00ea59f82d8e6328483ad3f29570cbdc385050f
Dylon Hill 18 <DylonHill18@newmanschool.org>
2017-10-10 19:34:30 -0500

    update

--- RAD code/lightsensor.py
+++ RAD code/lightsensor.py
@@ -1,8 +1,17 @@
-import RPi.GPIO as GPIO, time, os
+from pymongo import MongoClient
+import RPi.GPIO as GPIO
+import time
+import os
+import datetime
+
+client = MongoClient("mongodb://dylonhill18:hillsrock101@testcluster-shard-00-00-odrn0.mongodb.net:27017,testcluster-shard-00-01-odrn0.mongodb.net:27017,testcluster-shard-00-02-odrn0.mongodb.net:27017/test?ssl=true&replicaSet=testcluster-shard-0&authSource=admin")
+db = client.testing
+client.drop_database("testing")
+time.sleep(1)
 
 DEBUG = 1
 GPIO.setmode(GPIO.BCM)
-
+timestamp = datetime.datetime.now()
 def RCtime (RCpin):
         reading = 0
         GPIO.setup(RCpin, GPIO.OUT)
@@ -10,10 +19,26 @@ def RCtime (RCpin):
         time.sleep(1)
 
         GPIO.setup(RCpin, GPIO.IN)
-        # This takes about 1 millisecond per loop cycle
+
         while (GPIO.input(RCpin) == GPIO.LOW):
                 reading += 1
-        return reading
+        if reading < 5000:
+                status = "on"
+        else:
+                status = "off"
+
+        return status
 
 while True:
-        print RCtime(17) #This represents GPIO pin number that is recieveing the data
+        #print RCtime(4)
+        db.makerspace.insert(
+                {"timestamp": timestamp,
+                "lightstatus": RCtime(4),
+                "identifier": 1
+                }
+                )
+        makerspacedata = db.makerspace.find(
+                {"identifier": 1
+                }
+                )
+        print (makerspacedata)
